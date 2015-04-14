70c0d699bd2bbd8acfb5ca6d0c1b32a142deafe7
Vera Schlusmans <v.schlusmans@gmail.com>
2015-04-15 00:21:26 +0200

    tutoriel pygame clock

--- pygame_tutoriel2_clock.py
+++ pygame_tutoriel2_clock.py
@@ -12,7 +12,9 @@ screen = pygame.display.set_mode(size)          # crée une SURFACE (fenêtre gr
 speed = [1, 1]                                  # déplacement (en pixels) par tour, pour les directions x et y
 black = 0, 0, 0                                 # definir une couleur. tuple de rouge, vert, bleu  (valeurs entre 0 et 250)
 ball = pygame.image.load("girl.png")            # load retourne une autre surface, contenant l'image ball.bmp
-
+ballrect = ball.get_rect()                      # crée un objet rect (=zone rectangulaire)
+                                                # de la même dimension que ball
+                                                # il est encore vide
 
 while 1:                                        # crée une boucle infinie (1 est la même chose que True)
                                                 # c'est une "condition" qui est toujours vraie
@@ -23,23 +25,20 @@ while 1:                                        # crée une boucle infinie (1 es
         if event.type == pygame.QUIT:     # si le event est click sur la petite croix de fermeture,
             sys.exit()                    # on quitte le système, la boucle s'arrête donc
 
-    # WAS: deplacer le rectangle, qui va contenir l'image de la balle (on ne peut pas déplacer l'image même)
-    # deplacer l'image même
-    ball = ball.move(speed)             # à chaque tour de boucle on appelle la méthode move (WAS: des rectangles)
+    # deplacer le rectangle, qui va contenir l'image de la balle (on ne peut pas déplacer l'image même)
+    ballrect = ballrect.move(speed)             # à chaque tour de boucle on appelle la méthode move des rectangles
 
-    # WAS: si le rectangle dépasse les limites de l'écran on inverse sa direction
-    # si l'image dépasse les limites de l'écran on inverse sa direction
-    if ball.left < 0 or ball.right > width:
+    # si le rectangle dépasse les limites de l'écran on inverse sa direction
+    if ballrect.left < 0 or ballrect.right > width:
         speed[0] = -speed[0]                            # direction x
-    if ball.top < 0 or ball.bottom > height:
+    if ballrect.top < 0 or ballrect.bottom > height:
         speed[1] = -speed[1]                            # direction y
 
     # peindre tout en noir (pour effacer l'ancienne position)
     screen.fill(black)
     # blit = copier les pixels d'une surface sur une autre (ceci se passe en arrière-plan, dans la mémoire)
-    # WAS: screen.blit(ball, ballrect)         #  blit de l'image ball (source) dans ballrect (destination)
-    screen.blit(ball)
+    screen.blit(ball, ballrect)         # blit de l'image ball (source) dans ballrect (destination)
     # seulement quand cette copie est complète, le résultat est affiché à l'écran:
-    # pygame.display.flip()               # correspond à un nouveau frame dans un film
+    #pygame.display.flip()               # correspond à un nouveau frame dans un film
     pygame.display.update()             # remplace la ligne précédente
-    clock.tick(30)                      # max 30 frames par seconde (FPS)
+    clock.tick(30)  # max 30 frames per seconde (FPS)
